b4ea5d8eefb9b2629a43dc14dbc6bad2fb904b80
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-12 02:23:28 -0700

    add file extension

--- toolkitui/storage.py
+++ toolkitui/storage.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 
 import yaml
@@ -10,19 +11,19 @@ DATA_DIR = pathlib.Path(__file__).parent.parent / "data"
 
 def save_scenario(scenario: gato.entity.Scenario):
     key = f"scenario_{scenario.id}"
-    path = pathlib.Path(f"scenarios/{key}")
+    path = pathlib.Path(f"scenarios/{key}.yml")
     with open(DATA_DIR / path, "w") as file:
-        yaml.safe_dump(scenario.dict(), file)
+        yaml.safe_dump(json.loads(scenario.json()), file)
 
 
 def load_scenario(key) -> gato.entity.Scenario:
-    path = pathlib.Path(f"scenarios/{key}")
+    path = pathlib.Path(f"scenarios/{key}.yml")
     with open(DATA_DIR / path) as file:
         return gato.entity.Scenario(**yaml.safe_load(file))
 
 
 def save_action(scenario: gato.entity.Scenario, action: gato.entity.Action):
     key = f"action_{scenario.id}"
-    path = pathlib.Path(f"actions/{key}")
+    path = pathlib.Path(f"actions/{key}.yml")
     with open(DATA_DIR / path, "w") as file:
-        yaml.safe_dump(action.dict(), file)
+        yaml.safe_dump(json.loads(action.json()), file)
