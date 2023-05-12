8c7d9ce9b8c9c35f593d93b80b8513658967c9f1
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-12 02:19:21 -0700

    fix storage module

--- toolkitui/storage.py
+++ toolkitui/storage.py
@@ -5,18 +5,24 @@ import yaml
 import gato.entity
 
 
+DATA_DIR = pathlib.Path(__file__).parent.parent / "data"
+
+
 def save_scenario(scenario: gato.entity.Scenario):
     key = f"scenario_{scenario.id}"
-    with open(pathlib.Path(f"../data/scenarios/{key}")) as file:
+    path = pathlib.Path(f"scenarios/{key}")
+    with open(DATA_DIR / path, "w") as file:
         yaml.safe_dump(scenario.dict(), file)
 
 
 def load_scenario(key) -> gato.entity.Scenario:
-    with open(pathlib.Path(f"../data/scenarios/{key}")) as file:
+    path = pathlib.Path(f"scenarios/{key}")
+    with open(DATA_DIR / path) as file:
         return gato.entity.Scenario(**yaml.safe_load(file))
 
 
 def save_action(scenario: gato.entity.Scenario, action: gato.entity.Action):
     key = f"action_{scenario.id}"
-    with open(pathlib.Path(f"../data/actions/{key}")) as file:
+    path = pathlib.Path(f"actions/{key}")
+    with open(DATA_DIR / path, "w") as file:
         yaml.safe_dump(action.dict(), file)
