70178adf6120f776e04a8965391361e1d805c80e
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-12 19:29:37 -0700

    sort imports

--- toolkitui/executor.py
+++ toolkitui/executor.py
@@ -1,5 +1,6 @@
-from celery import Celery
 import os
+
+from celery import Celery
 
 broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
 result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
