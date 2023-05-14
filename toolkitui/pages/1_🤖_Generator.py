c616689c71ac2df5539538d8512ad1777b6ad354
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-14 00:26:37 -0700

    combine generation into single page with tabs

--- toolkitui/pages/1_🤖_Generator.py
+++ toolkitui/pages/1_🤖_Generator.py
@@ -2,15 +2,21 @@ import json
 import os
 import time
 
-import celery.result
 import gato.entity
 import gato.service
 import streamlit
+import celery.result
 
 from toolkitui import executor, storage
 
 
-def schedule_tasks(
+def schedule_scenario_tasks(
+        api_key: str, num_scenarios: int
+) -> list[celery.result.AsyncResult]:
+    return [executor.generate_scenario_task.delay(api_key)
+            for _ in range(num_scenarios)]
+
+def schedule_action_tasks(
         api_key: str,
         scenarios: list[gato.entity.Scenario],
 ) -> list[celery.result.AsyncResult]:
@@ -19,19 +25,19 @@ def schedule_tasks(
     ) for k, scenario in enumerate(scenarios)]
 
 
-def render_action(action: gato.entity.Action, container):
-    container.write(action.description)
-    container.write(f"Action ID: {action.id}")
+def render_scenario(scenario: gato.entity.Scenario, container):
+    container.write(scenario.description)
+    container.write(f"Scenario ID: {scenario.id}")
     container.divider()
 
 
-def update_progress(progress_bar, done, total):
-    if done == total:
+def update_progress(progress_bar, complete, total):
+    if complete == total:
         progress_text = f"Completed all {total} tasks."
     else:
-        progress_text = f"Completed {done} of {total} " \
+        progress_text = f"Completed {complete} of {total} " \
                         f"tasks. Please wait."
-    progress_bar.progress(done / total, text=progress_text)
+    progress_bar.progress(complete / total, text=progress_text)
 
 
 def get_complete_tasks(tasks):
@@ -50,10 +56,33 @@ def wait_for_results(progress_bar, tasks):
             time.sleep(0.1)
 
 
+def render_scenario_generator():
+    streamlit.header("Generate Scenarios")
+    num_scenarios = streamlit.number_input(
+        "Number of scenarios to generate",
+        min_value=1, value=1,
+    )
+    if streamlit.button("Generate Scenarios"):
+        with streamlit.spinner():
+            scenario_tasks = schedule_scenario_tasks(api_key, num_scenarios)
+
+        progress_text = f"Waiting for {num_scenarios} tasks. Please wait."
+        progress_bar = streamlit.progress(0, text=progress_text)
+        container = streamlit.container()
+        with streamlit.spinner():
+            for scenario in wait_for_results(progress_bar, scenario_tasks):
+                storage.save_scenario(scenario)
+                render_scenario(scenario, container)
+
+
+def render_action(action: gato.entity.Action, container):
+    container.write(action.description)
+    container.write(f"Action ID: {action.id}")
+    container.divider()
+
+
 def render_action_generator():
     streamlit.header("Generate Actions")
-    api_key = os.environ.get("OPENAI_API_KEY")
-    api_key = streamlit.text_input("OpenAI API Key", value=api_key, type="password")
     action_keys = storage.list_actions()
     complete_scenarios = {key.replace("action", "scenario") for key in action_keys}
     all_scenarios = set(storage.list_scenarios())
@@ -65,7 +94,7 @@ def render_action_generator():
     if streamlit.button("Generate Actions"):
         scenarios = list(filter(lambda s: s.id in choices, all_scenarios))
         with streamlit.spinner():
-            action_tasks = schedule_tasks(api_key, scenarios)
+            action_tasks = schedule_action_tasks(api_key, scenarios)
 
         num_actions = len(action_tasks)
         progress_text = f"Waiting for {num_actions} tasks. Please wait."
@@ -79,7 +108,13 @@ def render_action_generator():
 
 
 def main():
-    render_action_generator()
+    api_key = os.environ.get("OPENAI_API_KEY")
+    api_key = streamlit.text_input("OpenAI API Key", value=api_key, type="password")
+    tabs = streamlit.tabs(["Scenarios", "Actions"])
+    with tabs[0]:
+        render_scenario_generator()
+    with tabs[1]:
+        render_action_generator()
 
 
 if __name__ == '__main__':
